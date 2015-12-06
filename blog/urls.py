e4e92ed8fc0750c0b8f642d9e8454bcb63c64479
citizenara <hernnara@gmail.com>
2015-12-05 17:42:43 -0800

    addedtemple

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from .views import index
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls'))
+    url(r'', index )
 ]
